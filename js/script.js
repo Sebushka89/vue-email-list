7b05965604b105c21d0a0fd2ef872586b35fa6d6
Sebushka89 <sebastian.rizzo@hotmail.it>
2021-07-08 16:46:17 +0200

    done + bonus

--- js/script.js
+++ js/script.js
@@ -16,11 +16,13 @@ new Vue(
         methods: {
 
             getEmail: function () {
-                for(let i=0; i<10; i++)
-                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-                .then((response) => {  // appena il server ci risponde, si attiva questa funzione
+                for(let i=0; i<10; i++){
+                    axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                    .then((response) => {  // appena il server ci risponde, si attiva questa funzione
                     this.emails.push(response.data.response);
                 });
+            }
+                
             }
 
         }
